8a033f3f997014ef31867e26534ee0c87c4bf42c
薛惟仁 <wrxue@hj-rfid.com>
2020-06-12 08:26:53 +0800

    Using options to assign db and schema

--- linux/ubuntu/backupPgsql.sh
+++ linux/ubuntu/backupPgsql.sh
@@ -1,9 +1,37 @@
 #!/bin/bash
-if [ $# -ne 2 ]
-then
-    echo "Please input db name and schema name!"
-    exit 0
-fi
 
-pg_dump $1 -s --schema="$2" > "$(echo $(date +"%F %T") Schema.sql)"  
-pg_dump $1 -a --disable-triggers --column-inserts --schema="$2" > "$(echo $(date +"%F %T") Data.sql)"  
+while [ -n "$1" ]; do 
+    case "$1" in
+    --db);&
+    --db-name)  dbName="$2"
+                shift
+                ;;
+    --schema);&
+    --schema-name)  schemaName="$2"
+                    shift
+                ;;
+    *) echo "Option $1 not recognized" ;;
+
+    esac
+    shift
+done
+
+if [ -z "$dbName" ]; then
+    echo "No assign database name."
+    exit
+fi
+echo "DB name is $dbName"
+if [ -z "$schemaName" ]; then
+    echo "No assign schema name."
+    exit
+fi
+echo "Schema name is $schemaName"
+
+schemaBackup="pg_dump $dbName -s --schema=\"$schemaName\" > \"\$(echo \$(date +\"%F %T\") Schema_of_$dbName.$schemaName.sql)\""
+dataBackup="pg_dump $dbName -a --disable-triggers --column-inserts --schema=\"$schemaName\" > \"\$(echo \$(date +\"%F %T\") Data_of_$dbName.$schemaName.sql)\""
+
+eval "$schemaBackup"
+eval "$dataBackup"
+
+#echo "exec $schemaBackup"
+#echo "exec $dataBackup"
