448927a59e9954c26f172bfe811ddf0752df4c9b
weirenxue <32257183+weirenxue@users.noreply.github.com>
2020-09-24 22:53:05 +0800

    備份mysql的資料庫
    1. schema
    2. data
    3. 1+2

---
+++ linux/centos/scor_assi_backup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+dbName=scor_assi
+nowTime=$(date +"%F_%H-%M-%S")  #Schema_of_$dbName.sql
+
+schema_backup_filename=$(echo ${nowTime}_SchemaOf_${dbName}.sql)
+data_backup_filename=$(echo ${nowTime}_DataOf_${dbName}.sql)
+mix_backup_filename=$(echo ${nowTime}_AllOf_${dbName}.sql)
+
+schemaBackup="mysqldump -u wrxue -p --databases ${dbName} --default-character-set=utf8mb4 --no-data --result-file=${schema_backup_filename}"
+dataBackup="mysqldump -u wrxue -p --databases ${dbName} --default-character-set=utf8mb4 --no-create-info --no-create-db --result-file=${data_backup_filename}"
+mixBackup="mysqldump -u wrxue -p --databases ${dbName} --default-character-set=utf8mb4 --result-file=${mix_backup_filename}"
+
+eval "${schemaBackup}"
+eval "${dataBackup}"
+eval "${mixBackup}"                                                                                                           4,0-1         All
