61ce685a3288778566976f7c4f52065d4a54c6bd
weirenxue <32257183+weirenxue@users.noreply.github.com>
2020-09-25 12:35:46 +0800

    Create RemoveWin10DefultApplications.bat

---
+++ Windows/10/RemoveWin10DefultApplications.bat
@@ -0,0 +1,52 @@
+:: xbox
+PowerShell "Get-AppxPackage *xboxs* | Remove-AppxPackage" 
+:: VR入口
+PowerShell "Get-AppxPackage *MixedReality* | Remove-AppxPackage" 
+:: 3D檢視器
+PowerShell "Get-AppxPackage *Microsoft3DViewer* | Remove-AppxPackage"
+:: Get Offic
+PowerShell "Get-AppxPackage *MicrosoftOfficeHub * | Remove-AppxPackage"
+:: 接龍
+PowerShell "Get-AppxPackage *SolitaireCollection* | Remove-AppxPackage"
+:: 相片
+PowerShell "Get-AppxPackage *Photos* | Remove-AppxPackage"
+:: 微軟地圖
+PowerShell "Get-AppxPackage *Maps* | Remove-AppxPackage"
+:: 時鐘和鬧鐘
+PowerShell "Get-AppxPackage *Alarms* | Remove-AppxPackage"
+:: 聯絡人
+PowerShell "Get-AppxPackage *People* | Remove-AppxPackage"
+:: OneNote
+PowerShell "Get-AppxPackage *OneNote* | Remove-AppxPackage"
+:: 您的手機
+PowerShell "Get-AppxPackage *YourPhone* | Remove-AppxPackage"
+:: Get Skype
+PowerShell "Get-AppxPackage *SkypeApp* | Remove-AppxPackage"
+:: feedback
+PowerShell "Get-AppxPackage *FeedbackHub* | Remove-AppxPackage"
+:: 音樂
+PowerShell "Get-AppxPackage *ZuneMusic* | Remove-AppxPackage"
+:: 擷取與繪圖
+PowerShell "Get-AppxPackage *ScreenSketch* | Remove-AppxPackage"
+:: 郵件和行事曆
+PowerShell "Get-AppxPackage *windowscommunicationsapps* | Remove-AppxPackage" 
+:: 行動數據方案
+PowerShell "Get-AppxPackage *OneConnect* | Remove-AppxPackage" 
+::
+PowerShell "Get-AppxPackage *GetHelp* | Remove-AppxPackage"
+:: 提示
+PowerShell "Get-AppxPackage *Getstarted* | Remove-AppxPackage" 
+:: 電視與電影
+PowerShell "Get-AppxPackage *ZuneVideo* | Remove-AppxPackage"
+:: 訊息中心
+PowerShell "Get-AppxPackage *Messaging* | Remove-AppxPackage"
+::XboxApp遊戲中心
+PowerShell "Get-AppxPackage *XboxApp* | Remove-AppxPackage"
+::Xbox Game bar
+PowerShell "Get-AppxPackage *XboxGamingOverlay* | Remove-AppxPackage"
+::LinkedIn
+PowerShell "Get-AppxPackage *LinkedInforWindows* | Remove-AppxPackage"
+::Spotify
+PowerShell "Get-AppxPackage *SpotifyAB.SpotifyMusic* | Remove-AppxPackage"
+
+pause
