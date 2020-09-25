de4ed4b4e4b8ac07a955c1411a48c8f212bba879
weirenxue <32257183+weirenxue@users.noreply.github.com>
2020-09-25 14:47:49 +0800

    Update CollectData.bat

--- Windows/10/CollectData.bat
+++ Windows/10/CollectData.bat
@@ -6,8 +6,8 @@ set list_len=2
 
 
 for /l %%n in (1,1,%list_len%) do ( 
-	set DDLdir=.\!list[%%n]!_Related\DDL
-	set dir=.\!list[%%n]!_Related\DML
+	set DDLdir=.\Related_!list[%%n]!\DDL
+	set dir=.\Related_!list[%%n]!\DML
 	copy /y nul !dir!\Data_ALL.txt > nul
 	del !dir!\tmp.txt > nul 2>&1
 	for /F "tokens=5 delims=; " %%i in (!DDLdir!\Table_ALL_Drop.sql) do (
@@ -26,5 +26,11 @@ for /l %%n in (1,1,%list_len%) do (
 	)
 	del !dir!\Data_ALL.txt
 	echo !dir! ... done^^!
+)
+
+copy /y nul Data_ALL.sql > nul
+for /l %%n in (1,1,%list_len%) do ( 
+	set dir=.\Related_!list[%%n]!\DML
+	type !dir!\Data_ALL.sql>> Data_ALL.sql
 )
 pause
