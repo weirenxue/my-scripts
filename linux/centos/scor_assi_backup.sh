346f1cb5949a8e4e596afc4d5bfb21287c709eb6
weirenxue <32257183+weirenxue@users.noreply.github.com>
2020-09-25 08:19:23 +0800

    加上存放路徑
    加上路徑，在執行的使用者加目錄中的Documents/mysqlBackup/，
    這樣不論在哪執行此腳本，都會存到~/Documents/mysqlBackup/

--- linux/centos/scor_assi_backup.sh
+++ linux/centos/scor_assi_backup.sh
@@ -6,6 +6,9 @@ schema_backup_filename=$(echo ${nowTime}_SchemaOf_${dbName}.sql)
 data_backup_filename=$(echo ${nowTime}_DataOf_${dbName}.sql)
 mix_backup_filename=$(echo ${nowTime}_AllOf_${dbName}.sql)
 
+mkdir -p ~/Documents/mysqlBackup/
+cd ~/Documents/mysqlBackup/
+
 schemaBackup="mysqldump -u wrxue -p --databases ${dbName} --default-character-set=utf8mb4 --no-data --result-file=${schema_backup_filename}"
 dataBackup="mysqldump -u wrxue -p --databases ${dbName} --default-character-set=utf8mb4 --no-create-info --no-create-db --result-file=${data_backup_filename}"
 mixBackup="mysqldump -u wrxue -p --databases ${dbName} --default-character-set=utf8mb4 --result-file=${mix_backup_filename}"
