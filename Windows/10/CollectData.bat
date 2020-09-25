737232718f9e883123ad969eee6c2ec4167aa48d
weirenxue <32257183+weirenxue@users.noreply.github.com>
2020-09-25 14:41:12 +0800

    Create CollectData.bat
    Data在DML下，檔名為<tablename>.sql

---
+++ Windows/10/CollectData.bat
@@ -0,0 +1,30 @@
+@echo off
+setlocal enabledelayedexpansion 
+set list[1]=Privilege
+set list[2]=Department
+set list_len=2
+
+
+for /l %%n in (1,1,%list_len%) do ( 
+	set DDLdir=.\!list[%%n]!_Related\DDL
+	set dir=.\!list[%%n]!_Related\DML
+	copy /y nul !dir!\Data_ALL.txt > nul
+	del !dir!\tmp.txt > nul 2>&1
+	for /F "tokens=5 delims=; " %%i in (!DDLdir!\Table_ALL_Drop.sql) do (
+		echo !dir!\%%i.sql>> !dir!\tmp.txt
+		type !dir!\Data_ALL.txt>> !dir!\tmp.txt
+		del !dir!\Data_ALL.txt
+		ren !dir!\tmp.txt Data_ALL.txt
+	)
+	copy /y nul !dir!\Data_ALL.sql > nul
+	for /F "tokens=* delims=; " %%i in (!dir!\Data_ALL.txt) DO (
+		if exist %%i (
+			type %%i>> !dir!\Data_ALL.sql 
+			echo.>> !dir!\Data_ALL.sql  
+			echo.>> !dir!\Data_ALL.sql
+		)
+	)
+	del !dir!\Data_ALL.txt
+	echo !dir! ... done^^!
+)
+pause
